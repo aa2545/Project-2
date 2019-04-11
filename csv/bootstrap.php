4afe5014350aa0193d60b88d47447babe236311b
Alondra Avila <aa2545@njit.edu>
2019-04-11 11:14:37 -0400

    created a csv folder and test folder, also created a bootstrap php file and index html file

---
+++ csv/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lola
+ * Date: 4/11/2019
+ * Time: 11:07 AM
+ */
+
+namespace aa2545;
+
+
+class bootstrap
+{
+    public function__contruct{}
+    {
+        echo "test";
+    }
+}
