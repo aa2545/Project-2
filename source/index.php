dc332518a6bc0db496da75362ee6b8f173c096b2
Alondra Avila <aa2545@njit.edu>
2019-04-11 11:58:43 -0400

    Editing and refactored the index.php file

---
+++ source/index.php
@@ -0,0 +1,16 @@
+<?php
+namespace aa2545;
+require_once '../vendor/autoload.php';
+use aa2545\bootstrap;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF=8">
+    <title>CSC Import</title>
+</head>
+</html>
+<?php
+$obj = new bootstrap();
+?>
